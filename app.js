f332e73e16efc402acb9a7019765347763a01c68
Maggie Saldivia <Maggiesaldivia@gmail.com>
2021-08-02 18:53:00 -0700

    Creating and rendering nested React components

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-var GroceryList = () => (
+var GroceryList = () => {
+  return (
   <div>
-    <li>carrots</li>
-    <li>bread</li>
+    <p>Grocery List: </p>
+    <ul>
+      <Bread />
+      <Carrot />
+    </ul>
   </div>
-)
+  );
+},
+Bread = () => {
+  return (
+    <li>bread</li>
+  )
+},
+Carrot = () => {
+  return (
+    <li>carrot</li>
+  )
+};
 
 ReactDOM.render(<GroceryList />, document.getElementById('app'))
